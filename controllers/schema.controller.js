040f38725acc54a95472d1e7934edb0a42f4dd7f
yj3006 <yj3006@gmail.com>
2024-12-26 14:28:07 +0800

    Update schema.controller.js

--- controllers/schema.controller.js
+++ controllers/schema.controller.js
@@ -11,9 +11,14 @@ var logger = require('../logging.js');
 //basic-auth is used to validate basic authentication
 var auth = require('basic-auth');
 
-//This variable is used to generate the ID and externalId for a new user entry in
-// the userdb.json file.
-var sequenceNumber = 10;
+const coreUserSchema = {
+  id: "urn:ietf:params:scim:schemas:core:2.0:User",
+  name: "User",
+  description: "Core User schema",
+  attributes: [
+  ]
+};
+
 
 //This function returns the schema for the target system.
 exports.findAll = function (req, res) {
@@ -26,28 +31,14 @@ exports.findAll = function (req, res) {
   logger.log(req.query); // Log the query parameters for debugging
   logger.log('----');
 
+  
   // Check credentials
   if (users.authenticate(req, res)) {
-    const userIDStore = JSON.parse(fs.readFileSync('./schemas/starsuite-user-extension-schema.json', 'utf8'));
-    let filteredUsers = userIDStore.resources;
+    const schemaExtension = JSON.parse(fs.readFileSync('./schemas/starsuite-user-extension-schema.json', 'utf8'));
 
-    // Apply filters from query parameters
-    if (req.query.filter) {
-      const filter = decodeURIComponent(req.query.filter);
-
-      // Example: Parsing `userName eq "value"`
-      const match = filter.match(/(\w+)\s+eq\s+"(.+?)"/);
-      if (match) {
-        const field = match[1];
-        const value = match[2];
-        filteredUsers = filteredUsers.filter(user => user[field] === value);
-      } else {
-        logger.log('Invalid filter format');
-        return res.status(400).json({ error: 'Invalid filter format' });
-      }
-    }
+    
 
     logger.log('Found ' + filteredUsers.length + ' users.');
-    res.status(200).json({ resources: filteredUsers });
+    res.status(200).json([coreUserSchema, schemaExtension]);
   }
 };
