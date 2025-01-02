bb6079c42bc6cd6ccf271b1677ee2e8d5cdcbda3
yj3006 <yj3006@gmail.com>
2025-01-02 10:21:56 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,21 @@ module.exports = function(app) {
   var users = require('../controllers/user.controller.js');
 
   //Get operation for /Users endpoint
-  app.get('/scimgate/Users', users.findAll);
+  app.get('/scim/Users', users.findAll);
 
   //Get operation for /Users endpoint
-  app.get('/scimgate/Users/:id', users.findOne);
+  app.get('/scim/Users/:id', users.findOne);
 
   //Post operation for /Users endpoint
-  app.post('/scimgate/Users', users.create);
+  app.post('/scim/Users', users.create);
 
   //Put and Patch operation for /Users endpoint
-  app.put('/scimgate/Users/:id', users.update);
-  app.patch('/scimgate/Users/:id', users.update);
+  app.put('/scim/Users/:id', users.update);
+  app.patch('/scim/Users/:id', users.update);
 
   //Delete operation for /Users endpoint
-  app.delete('/scimgate/Users/:id', users.delete);
+  app.delete('/scim/Users/:id', users.delete);
 
   
-  app.get('/scimgate/', users.test);
+  app.get('/scim/', users.test);
 }
