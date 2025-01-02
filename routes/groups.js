ac814d3269b93f6741709b632c6fd070132e7eef
yj3006 <yj3006@gmail.com>
2025-01-02 10:21:18 +0800

    Update groups.js

--- routes/groups.js
+++ routes/groups.js
@@ -5,19 +5,19 @@ module.exports = function(app){
   var groups = require('../controllers/group.controller.js');
 
   //Get operation for /Groups endpoint
-  app.get('/scimgate/Groups', groups.findAll);
+  app.get('/scim/Groups', groups.findAll);
 
   //Get operation for /Groups endpoint
-  app.get('/scimgate/Groups/:id', groups.findOne);
+  app.get('/scim/Groups/:id', groups.findOne);
 
   //Post operation for /Groups endpoint
-  app.post('/scimgate/Groups', groups.create);
+  app.post('/scim/Groups', groups.create);
 
   //Put and Patch operation for /Groups endpoint
-  app.put('/scimgate/Groups/:id', groups.update);
-  app.patch('/scimgate/Groups/:id', groups.update);
+  app.put('/scim/Groups/:id', groups.update);
+  app.patch('/scim/Groups/:id', groups.update);
 
   //Delete operation for /Groups endpoint
-  app.delete('/scimgate/Groups/:id', groups.delete);
+  app.delete('/scim/Groups/:id', groups.delete);
 
 }
